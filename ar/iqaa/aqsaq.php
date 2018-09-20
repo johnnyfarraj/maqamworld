1607c337bd66f5157019970c63ae1d902c7d3839
Johnny Farraj <johnny.farraj@thomsonreuters.com>
2018-09-20 13:29:42 -0400

    finished aqsaq

--- ar/iqaa/aqsaq.php
+++ ar/iqaa/aqsaq.php
@@ -38,8 +38,11 @@ include($ROOT . 'inc/head.php');
                         <img src="../note/iqaa/aqsaq_f1.png" class="img-fluid">
                     </div>
 
-                    <p>Iqa‘ Aqsaq is very popular in the <a href="../form/vocal_comp.php#muwashah">Muwashah</a> vocal genre. It is 1 beat short of <a href="samai_thaqil.php">Iqa‘ Sama‘i Thaqil</a> but sometimes it can be played immediately before or after a piece in that iqa‘ without interruption, during a Wasla. An alternate form of Aqsaq replaces the rest on the 6<sup>the</sup> beat with another <em>dum</em>, thereby creating a sequence of 2 consecutive <em>dum</em>-s that evokes <a href="samai_thaqil.php">Iqa‘ Sama‘i Thaqil</a>.
-					</p>
+                    <h5>
+                    إيقاع اقصاق يُسنعمل كثيراَ في قالب <a href="../form/vocal_comp.php#muwashah">الموشح</a> الغنائي.
+                    وهو يشبه <a href="samai_thaqil.php">إيقاع سماعي ثقيل</a> لكن ينقصه ضربة واحدة من اصل عشرة، ولذا كثيراَ ما يأتي هذا الإيقاع من قبل او بعد قطعة من إيقاع سماعي ثقيل بلا توقف العزف, ضمن قالب الوصلة.
+					</h5>
+					<br>
 
                     <div class="player-area">
                         <audio id="player" controls>
@@ -54,7 +57,7 @@ include($ROOT . 'inc/head.php');
                                 <div class="info">
                                     <b>موشح ما احتيالي</b>
                                     <span>الاب لويس الحاج وعايدة شلهوب</span>
-                                    <span><img src="/img/cd.png"> Mouwashah - Chants Arabo-andalous</span>
+                                    <span><img src="/img/cd.png"> اسطوانة الموشّح - الاغاني العربية الاندلسية</span>
                                 </div>
                             </label>
                         </div>
@@ -72,7 +75,7 @@ include($ROOT . 'inc/head.php');
                             </label>
                         </div>
                     </div>
-                    
+
                     <div class="track" data-song="/audio/iqaa/aqsaq_form1/jalla_man_bil_husni.mp3">
                         <div class="radio">
                             <label>
@@ -86,7 +89,7 @@ include($ROOT . 'inc/head.php');
                             </label>
                         </div>
                     </div>
-                    
+
                     <div class="track" data-song="/audio/iqaa/aqsaq_form1/bashraf_bulbul_al_afrah.mp3">
                         <div class="radio">
                             <label>
@@ -99,13 +102,16 @@ include($ROOT . 'inc/head.php');
                             </label>
                         </div>
                     </div>
-                    
+
                     <!-- Need some kind of divider here to start the Form 2  examples -->
                     <br>
                     <div class="heading jins text-center">
                         <h3>الشكل الثاني لايقاع اقصاق</h3>
                     </div>
                     <div class="clearfix"></div>
+                    <h5>يأتي إيقاع اقصاق على شكلٍ ثانٍ كما هو مدوّن ادناه، حيث تُستبدل علامة الإسّ على الضربة السادسة بعلامة دُمّ.
+                    وبذلك يحصل هذا الشكل من إيقاع اقصاق على دمّين متتاليتين تزيده شبهاً بإيقاع <a href="samai_thaqil.php">سماعي ثقيل</a>.
+					</h5>
                     <br>
 
                     <div class="notation">
@@ -120,12 +126,12 @@ include($ROOT . 'inc/head.php');
                                     <b>موشّح ماحتيالي</b>
                                     <span>فرقة التراث (حلب)</span>
                                     <span>بقيادة محمد حمّادية</span>
-                                    <span><img src="/img/cd.png"> The Music in Spanish Al Andalus - Hijaz</span>
+                                    <span><img src="/img/cd.png"> اسطوانة موسيقى الاندلس الاسبانية - مقام الحجاز</span>
                                 </div>
                             </label>
                         </div>
                     </div>
-                    
+
                     <div class="track" data-song="/audio/iqaa/aqsaq_form2/ayyuha_al_saqi.mp3">
                         <div class="radio">
                             <label>
@@ -138,7 +144,7 @@ include($ROOT . 'inc/head.php');
                             </label>
                         </div>
                     </div>
-                    
+
                     <div class="track" data-song="/audio/iqaa/aqsaq_form2/azzibuni.mp3">
                         <div class="radio">
                             <label>
@@ -147,12 +153,12 @@ include($ROOT . 'inc/head.php');
                                     <b>موشّح عذّبوني</b>
                                     <span>فرقة الكندي</span>
                                     <span>الحان عمر البطش (حلب)</span>
-                                    <span><img src="/img/cd.png"> The Aleppian Music Room Vol. 1 - Hijazkar Kurd &amp; Saba</span>
+                                    <span><img src="/img/cd.png"> حجرة الموسيقى االحلبية – الجزء الاول (<a href="../maqam/hijazkar_kurd.php">مقام حجازكار كرد</a>)</span>
                                 </div>
                             </label>
                         </div>
                     </div>
-                    
+
                 </div>
                 <div class="col-sm-12">
                     <?php include($ROOT . 'inc/768x90.php'); ?>
@@ -160,9 +166,9 @@ include($ROOT . 'inc/head.php');
             </div>
         </div>
     </div>
-    
+
     <?php include($ROOT . 'ar/footer.php'); ?>
     <?php include($ROOT . 'inc/javascriptFiles.php'); ?>
-    
+
   </body>
 </html>
