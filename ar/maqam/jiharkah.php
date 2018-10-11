8d17b4acb9d89b533dd0558adc7b727e08193059
Johnny Farraj <johnnyfarraj@gmail.com>
2018-10-11 16:27:29 -0400

    added example

--- ar/maqam/jiharkah.php
+++ ar/maqam/jiharkah.php
@@ -78,6 +78,19 @@ include($ROOT . 'inc/head.php');
                         </div>
                     </div>
 
+                   <div class="track " data-song="/audio/maqam/jiharkah/3ishna_w_shufna.mp3">
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="song" value="1" >
+                                <div class="info">
+                                    <b>Dawr ‘Ishna w Shufna</b>
+                                    <span>Saleh Abdel Hayy</span>
+                                    <span>الحان محمد عثمان (مصر)</span>
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+
                     <div class="track" data-song="/audio/maqam/jiharkah/samai_jiharkah.mp3">
                         <div class="radio">
                             <label>
--- en/maqam/jiharkah.php
+++ en/maqam/jiharkah.php
@@ -83,6 +83,19 @@ include($ROOT . 'inc/head.php');
                         </div>
                     </div>
 
+                    <div class="track " data-song="/audio/maqam/jiharkah/3ishna_w_shufna.mp3">
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="song" value="1" >
+                                <div class="info">
+                                    <b>Dawr ‘Ishna w Shufna</b>
+                                    <span>Saleh Abdel Hayy</span>
+                                    <span>Music by Muhammad ‘Uthman</span>
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+
                     <div class="track" data-song="/audio/maqam/jiharkah/samai_jiharkah.mp3">
                         <div class="radio">
                             <label>
