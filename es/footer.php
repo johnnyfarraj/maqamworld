b95186cad5391611ab53049da2a9a8fa0b75474e
Johnny Farraj <johnny.farraj@thomsonreuters.com>
2018-10-23 11:07:31 -0400

    some spanish updates

--- es/footer.php
+++ es/footer.php
@@ -4,9 +4,9 @@
         <div class="row">
             <div class="col-sm-4"> <img src="/img/logo-bottom.png" height="30" class="footer-logo">
                 <p>&copy; 2001-2018 MaqamWorld
-                    <br>Alle Rechte vorbehalten.
-                    <br>Design und Programmierung: <a href="http://filiptodorov.com" target="_blank">Filip Todorov</a>
-                    <br>Deutsche Übersetzung: Ines Körver
+                    <br>Todos los derechos reservados.
+                    <br>Diseño y programación: <a href="http://filiptodorov.com" target="_blank">Filip Todorov</a>
+                    <br>Traducción al español: Aaron L Rosenberg
                 </p>
                 <ul class="socials">
                     <li><a href="#"><i class="fa fa-fw fa-facebook"></i></a></li>
@@ -16,11 +16,11 @@
                 </ul>
             </div>
             <div class="col-sm-3">
-                <h3>Kontakt</h3>
+                <h3>Contacto</h3>
                 <ul class="menu">
-                    <li><a href="#"><i class="fa fa-fw fa-phone"></i>Rufen Sie uns an</a></li>
-                    <li><a href="#"><i class="fa fa-fw fa-map-marker"></i>Besuchen Sie uns</a></li>
-                    <li><a href="/es/contact.php"><i class="fa fa-fw fa-envelope"></i>Schreiben Sie uns</a></li>
+                    <li><a href="#"><i class="fa fa-fw fa-phone"></i>Teléfono</a></li>
+                    <li><a href="#"><i class="fa fa-fw fa-map-marker"></i>Dirección</a></li>
+                    <li><a href="/es/contact.php"><i class="fa fa-fw fa-envelope"></i>Correo electrónico</a></li>
                 </ul>
             </div>
             <div class="col-sm-5 text-right">
