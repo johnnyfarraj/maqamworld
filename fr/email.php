609670f4830f1ffe5c9e747e6d486ef510e89936
Johnny Farraj <johnny.farraj@thomsonreuters.com>
2019-08-13 12:20:15 -0400

    fixed french email problem

--- fr/email.php
+++ fr/email.php
@@ -7,20 +7,20 @@ $email = strip_tags($_POST['email']);
 $ms = strip_tags($_POST['message']);
 
 if(isset($firstname) && $firstname != "" && isset($lastname) && $lastname != "" && isset($email) && $email != "" && isset($ms) && $ms != "") {
-
+    
     /* PHP MAIl FUNCTION */
     $to      = 'johnnyfarraj@gmail.com';
     $subject = 'Nouveau message de MaqamWorld';
-    $message = 'Bonjour,<br><br>Vous avez un nouveau message de MaqamWorld:<br><br>Name: '.$firstname.' '.$lastname.'<br>Email: '.$email.'<br>Message: '.$ms;
+    $message = 'Bonjour,<br><br>Vous avez un nouveau message de MaqamWorld:<br><br>Nom: '.$firstname.' '.$lastname.'<br>Email: '.$email.'<br>Message: '.$ms;
     $headers = 'From: no-reply@maqamworld.com' . "\r\n" .
     'Reply-To: johnnyfarraj@gmail.com' . "\r\n" .
     'Content-Type: text/html; charset=UTF-8\r\n';
-
+    
     mail($to, $subject, $message, $headers);
-
-    echo json_encode(array("status" => "success", "message" => "Merci de nous avoir contactés. Nous tacherons de vous répondre dans les prochaines 24 - 48 heures."));
+    
+    echo json_encode(array("status" => "success", "message" => "Merci de nous avoir contactÃ©s. Nous tacherons de vous rÃ©pondre dans les prochaines 24 - 48 heures."));
 } else {
-    echo json_encode(array("status" => "error", "message" => "Prière de tout remplir avant d'envoyer le document."));
+    echo json_encode(array("status" => "error", "message" => "PriÃ¨re de tout remplir avant d'envoyer le document."));
 }
 
 ?>
