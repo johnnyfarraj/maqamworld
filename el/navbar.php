50053c9d17d354269c8501b20bd73748ad983e5d
Johnny Farraj <38724834+johnnyfarraj@users.noreply.github.com>
2020-03-24 16:42:34 -0400

    navbar

---
+++ el/navbar.php
@@ -0,0 +1,26 @@
+<!-- Navbar -->
+<div class="collapse navbar-collapse" id="navbarDefault">
+	<ul class="navbar-nav mr-auto">
+		<li class="nav-item <?php if($page == "el/index.php") { echo "active"; } ?>">
+			<a class="nav-link" href="/en/index.php"><i class="fa fa-fw fa-home"></i></a>
+		</li>
+		<li class="nav-item <?php if($page == "el/instr.php") { echo "active"; } ?>">
+			<a class="nav-link" href="/en/instr.php">Mουσικά Όργανα</a>
+		</li>
+		<li class="nav-item <?php if($page == "el/forms.php") { echo "active"; } ?>">
+			<a class="nav-link" href="/en/forms.php">Φόρμες</a>
+		</li>
+		<li class="nav-item <?php if($page == "el/jins.php") { echo "active"; } ?>">
+			<a class="nav-link" href="/en/jins.php">Γένη</a>
+		</li>
+		<li class="nav-item <?php if($page == "el/maqam.php") { echo "active"; } ?>">
+			<a class="nav-link" href="/en/maqam.php">Μακάμ</a>
+		</li>
+		<li class="nav-item <?php if($page == "el/iqaa.php") { echo "active"; } ?>">
+			<a class="nav-link" href="/en/iqaa.php">Ρυθμοί</a>
+		</li>
+		<li class="nav-item <?php if($page == "el/book.php") { echo "active"; } ?>">
+			<a class="nav-link" href="/en/book.php">Βιβλίο</a>
+		</li>
+	</ul>
+</div>
