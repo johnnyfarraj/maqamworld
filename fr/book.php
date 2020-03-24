9a574eaf14d5f9fc08feade7a81a74871957c084
Johnny Farraj <38724834+johnnyfarraj@users.noreply.github.com>
2020-03-24 10:26:15 -0400

    updated book release date

--- fr/book.php
+++ fr/book.php
@@ -30,12 +30,14 @@ include($ROOT . 'inc/head.php');
                     </div>
                     <div class="clearfix"></div>
 
-                    <p>Je suis heureux d'annoncer mon nouveau livre <strong>Inside Arabic Music</strong> co-écrit avec mon ami Sami Abou Shumays (créateur de <a href="http://maqamlessons.com" target="_blank">MaqamLessons</a>). Ce nouveau livre a été publié par les <a href="http://oup.com" target="_blank">Presses de l'Université Oxford</a> en Août 2019. Ce livre sera une référence complète sur la musique arabe et accompagnera ce site web.
+                    <p>Je suis très heureux d'annoncer un nouveau livre <strong>Inside Arabic Music</strong> co-écrit avec mon ami Sami Abou Shumays (créateur de <a href="http://maqamlessons.com" target="_blank">MaqamLessons</a>). 
+                      Ce nouveau livre a été publié par les <a href="http://oup.com" target="_blank">Presses de l'Université Oxford</a> en Août 2019, et sera une référence complète sur la musique arabe et accompagnera ce site web.
                     </p>
                     <p><strong>Inside Arabic Music</strong> a reçu une bourse en 2012 de <a href="http://arabculturefund.org/grantees/grantee.php?id=333" target="_blank">la Fondation Arabe pour les Arts et la Culture</a>.
                     </p>
                     <p style="margin-bottom:40px;">
-                    Vous pouves commander une copie de <strong>Inside Arabic Music</strong> de chez <a href="https://www.amazon.com/Inside-Arabic-Music-Performance-Century/dp/0190658363/ref=sr_1_1" target="_blank">Amazon.com</a> ou directement de chez l'éditeur <a href="https://global.oup.com/academic/product/inside-arabic-music-9780190658366" target="_blank">Oxford University Press</a>.
+                    Vous pouvez commander une copie de <strong>Inside Arabic Music</strong> de chez <a href="https://www.amazon.com/Inside-Arabic-Music-Performance-Century/dp/0190658363/ref=sr_1_1" target="_blank">Amazon.com</a> 
+                      ou directement de chez l'éditeur <a href="https://global.oup.com/academic/product/inside-arabic-music-9780190658366" target="_blank">Oxford University Press</a>.
                     </p>
                     
                     <div class="row">
