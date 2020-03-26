886b5bd68945d991c81dec249fb82508af26c6a4
Farraj <Johnny.Farraj@thomsonreuters.com>
2020-03-26 16:38:41 -0400

    greek

--- el/instr.php
+++ el/instr.php
@@ -29,109 +29,96 @@ include($ROOT . 'inc/head.php');
                 <div class="col-md-9 col-sm-9">
 
                     <div class="heading text-center jins">
-                        <h1>Instruments Musicaux Arabes</h1>
-                        <h3>Instruments Traditionnels, Arabisés et Percussions</h3>
+                        <h1>Αραβικά Μουσικά Όργανα</h1>
+                        <h3>Παραδοσιακά, δυτικά Αραβοποιημένα και κρουστά</h3>
                     </div>
                     <a href="#" class="show-side-menu d-block d-md-none btn btn-primary"><i class="fa fa-fw fa-bars"></i>Index des Instruments</a>
                     <div class="clearfix"></div>
 
                     <div class="text-center mt-2 mb-3 mobile-left">
-                        <a href="#" class="pronunciation btn btn-default" data-audio="/name/takht.mp3"><i class="fa fa-fw fa-volume-up"></i> Prononciation de Takht</a>
+                        <a href="#" class="pronunciation btn btn-default" data-audio="/name/takht.mp3"><i class="fa fa-fw fa-volume-up"></i> Προφορά του Ταχτ</a>
                     </div>
                     <div class="text-center mt-2 mb-3 mobile-left">
-                        <a href="#" class="pronunciation btn btn-default" data-audio="/name/sahb.mp3"><i class="fa fa-fw fa-volume-up"></i> Prononciation de Sahb</a>
+                        <a href="#" class="pronunciation btn btn-default" data-audio="/name/sahb.mp3"><i class="fa fa-fw fa-volume-up"></i> Προφορά του Σαχμπ</a>
                     </div>
                     <div class="text-center mt-2 mb-3 mobile-left">
-                        <a href="#" class="pronunciation btn btn-default" data-audio="/name/naqr.mp3"><i class="fa fa-fw fa-volume-up"></i> Prononciation de Naqr</a>
+                        <a href="#" class="pronunciation btn btn-default" data-audio="/name/naqr.mp3"><i class="fa fa-fw fa-volume-up"></i> Προφορά του Νακρ</a>
                     </div>
 
-                    <p id="takht">L'ensemble traditionnel arabe ou takht consiste en quatre instruments mélodiques:
-		le <a href="instr/oud.php">oud</a>, le <a href="instr/nay.php">nay</a>, le <a href="instr/qanun.php">qanoun</a>
-		 et le <a href="instr/violin.php">violon</a>, et en l'instrument de percussion principal: le <a href="instr/riqq.php">riqq</a>.
-		Des fois, au riqq est ajouté (ou le riqq est remplacé par) la <a href="instr/tabla.php">tabla</a>
-		ou le <a href="instr/daff.php">daff</a> (tambour sur cadre),
-		et aux instruments mélodiques peut être ajouté le <a href="instr/buzuq.php">bouzouk</a>.
+                    <p id="takht">
+                        Το τυπικό παραδοσιακό Αραβικό μουσικό σύνολο ονομάζεται Ταχκτ και αποτελείται από τεσσερα κυρίως όργανα: το <a href="instr/oud.php">ούτι</a>, το <a href="instr/nay.php">νάι</a>, το <a href="instr/qanun.php">κανονάκι</a> και το <a href="instr/violin.php">βιολί</a>, και ένα βασικό κρουστό: το <a href="instr/riqq.php">ρεκ</a>. Κάποιες φορές το ρεκ αντικαθίσταται/συμπληρώνεται με την <a href="instr/tabla.php">τάμπλα</a> (όρος συχνά χρησιμοποιημένος αντί τoν όρο νταρμπούκα ή ντούμπεκ. Μην το συγχέετε με την Ινδική τάμπλα) ή το <a href="instr/daff.php">νταφ</a> (είδος μπεντίρ), και τα μελωδικά όργανα μπορούν να συμπληρωθούν με το <a href="instr/buzuq.php">μπούζουκ</a>.
                     </p>
 
                     <p>
-		Au niveau stylistique, les instruments mélodiques sont divisés en deux familles:
-		<em>sahb</em> (du mot "tirer"; instruments à cordes frottées et à vent, aux sons longs)
-		et <em>naqr</em> (du mot "tapper"; instruments à cordes pincées ou frappées, aux sons courts).
-		Le violon et le nay appartiennent à la catégorie sahb, tandis que le oud et le qanoun appartiennent à la catégorie naqr.
-		Les deux familles ont un rôle complémentaire permettant de créer un son plus riche et plus complexe.
+                        Στυλιστικά, τα μελωδικά όργανα χωρίζονται σε δύο οικογένειες τεχνικών χαρακτηριστικών: <em>Σαχμπ</em> (όργανα που παραγουν νότες διαρκείας), όπως το βιολί και το νάι, και <em>Νακρ</em> (νυκτά), όπως το ούτι και κανονάκι. 
+                        Οι δύο αυτές οικογένειες αλληλοσυμπληρώνουν η μία την άλλη ώστε το ηχητικό αποτελεσμα να είναι πλουσιότερο και πιο ολοκληρωμένο.
                     </p>
 
                     <p>
-		Les instruments arabisés sont des instruments non-arabes qui sont entrés dans l'ensemble de musique arabe au 20<sup>ième</sup> siècle et sont devenus partie prenante du son de la musique arabe de cette période.
-		La plupart de ces instruments ont été adoptés en Égypte où les musiciens ont développé de nouvelles techniques et manières d'ornementer qui ont permis à ces instruments de sonner 'arabe'.
-        Certains de ces instruments ont été modifiés de manière à ajouter de nouvelles notes de manière à être en mesure de donner une version assez proche de l'intonation correcte du <a href="../maqam.php">maqam</a> arabe.
-		Certains autres instruments possédant un système de 12 notes également tempérées ont été utilisés sans transformation.
-		</p>
+                        Αραβοποιημένα όργανα είναι τα μη-Αραβικά όργανα που εισήλθαν στην Αραβική μουσική τον εικοστό αιώνα και γίνανε μέρος του Αραβικού ήχου εκείνης της εποχής. 
+                        Τα περισσότερα από αυτά τα όργανα υπήρξαν καινοτόμα στην Αίγυπτο, όπου οι μουσικοί ανέπτυξαν νέες μορφές μελωδικής διάνθισης και τεχνικής που κάναν αυτά τα όργανα να ακούγονται εντελώς Αράβικα.
+                        Κάποια αραβοποιημένα όργανα υπέστησαν τεχνική διαφοροποίηση κατασκευαστικά ώστε να προσαρμοστούν στο απαιτούμενα Αραβικά διαστήματα. 
+                        Άλλα οργανα, λόγω αδυναμίας τεχνικής παρέμβασης παρέμειναν ως έχουν με το 12τονο ισοσκελισμένο τρόπο με τον οποίο κατασεκυάστηκαν εξ αρχής.
+		              </p>
 
-        <p>
-		Cette catégorie inclue le <a href="instr/piano.php">piano</a>, le piano électrique,
-		l'<a href="instr/org.php">orgue électrique</a>, le <a href="instr/keyboard.php">synthétiseur oriental</a>,
-		l'<a href="instr/accordion.php">accordéon</a>, la <a href="instr/guitar.php">guitare électrique</a> et la basse électrique (sans frette).
-		Certains de ces instruments peuvent être modifiés de manière à produire les quarts de ton.
-		La batterie et la percussion électronique sont aussi souvent utilisées pour la musique arabe contemporaine populaire ou de danse.
-        </p>
+                    <p>
+                        Σε αυτα συμπεριλαμβάνονται το <a href="instr/piano.php">πιάνο</a>, το ηλεκτρικό πιάνο, το <a href="instr/org.php">ηλεκτρικό όργανο</a> ή αρμόνιο, το <a href="instr/keyboard.php">Oriental keyboard</a>, το <a href="instr/accordion.php">ακκορντεόν</a>, η <a href="instr/guitar.php">ηλεκτρική κιθάρα</a>, και το ηλεκτρικό ταστωμένο μπάσσο. 
+                        Κάποια από αυτά τα όργανα μπορούν να προσαρμοστούν και να παράγουν τέταρτα του τόνου. Η ντραμς και τα ηλεκτρονικά κρουστά επίσης χρησιμοποιούνται συχνά σε πιο μοντέρνα Αραβική ποπ/χορευτική μουσική.
+                    </p>
 
-        <p>La flute, le <a href="instr/saxophone.php">saxophone</a>, la clarinette et la <a href="instr/trumpet.php">trompette</a> sont
-		des instruments à tempérament non-égal et permettent un plus grand contrôle des fréquences
-		que les instruments venant d'être mentionnés. Ils sont utilisés à des degrés divers dans la musique arabe,
-		particulièrement dans les styles de jazz fusion arabe modernes.
-        </p>
+                    <p>
+                        Το φλάουτο, <a href="instr/saxophone.php">σαξόφωνο</a>, κλαρινέτο και <a href="instr/trumpet.php">τρομπέτα</a> δεν εχουν απο την φύση λειτουργίας τους περιορισμό του 12τονου διαχωρισμού και έτσι επιτρέπουν μεγαλύτερο έλεγχο πάνω στο κούρδισμα σε σχέση με τα υπόλοιπα δυτικά όργανα. Αυτα τα όργανα χρησιμοποιούνται σε ποικίλες επεκτάσεις στυλιστικές , ειδικά σε μοντέρνα Αραβικά jazz fusion είδη.
+                    </p>
 
-        <p>Le violon, l'alto, le violoncelle et la contrebasse sont beaucoup utilisés dans la musique arabe,
-		particulièrement dans les grands ensembles comme celui de Oum Koulthoum et l'Orchestre Diamant en Égypte.
-		Étant tous sans frette, ces instruments peuvent produire sans problème les gammes du <a href="maqam.php">maqam</a> arabe.
-        </p>
+                    <p>
+                        Το βιολί, βιόλα, τσέλο, και κοντραμπάσσο είναι τα πλέον διαδεδομένα όργανα στην Αραβική μουσική, ειδικά στις μεγάλες ορχήστρες όπως η Ουμ Καλθούμ Ορχήστρα και η Diamond Orchestra στην Αίγυπτο. Λόγω του ότι είναι άταστα όργανα, τα έγχορδα αυτά άνετα χρησιμοποιούνται χωρίς περιορισμό τα Αραβικά <a href="maqam.php">μακάμια</a>.
+                    </p>
 
-        <div class="row">
-            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                <div class="card">
-                    <img class="card-img-top" src="/img/rosette_khalaf.png" alt="Oud Rosette Khalaf">
-                    <div class="card-body">
-                        <h5 class="card-title">Instruments Traditionnels</h5>
-                        <p class="card-text"><a href="instr/oud.php">Το Ούτι</a></p>
-                        <p class="card-text"><a href="instr/qanun.php">Το Κανονάκι</a></p>
-                        <p class="card-text"><a href="instr/nay.php">Το Νάι</a></p>
-                        <p class="card-text"><a href="instr/violin.php">Το Αραβικό Bιολί</a></p>
-                        <p class="card-text"><a href="instr/buzuq.php">Το Μπούζουκ</a></p>
+                <div class="row">
+                    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                        <div class="card">
+                            <img class="card-img-top" src="/img/rosette_khalaf.png" alt="Oud Rosette Khalaf">
+                            <div class="card-body">
+                                <h5 class="card-title">Instruments Traditionnels</h5>
+                                <p class="card-text"><a href="instr/oud.php">Το Ούτι</a></p>
+                                <p class="card-text"><a href="instr/qanun.php">Το Κανονάκι</a></p>
+                                <p class="card-text"><a href="instr/nay.php">Το Νάι</a></p>
+                                <p class="card-text"><a href="instr/violin.php">Το Bιολί</a></p>
+                                <p class="card-text"><a href="instr/buzuq.php">Το Μπούζουκ</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                        <div class="card">
+                            <img class="card-img-top" src="/img/nahat_girgi.png" alt="Oud Rosette Nahhat">
+                            <div class="card-body">
+                                <h5 class="card-title">Instruments Arabisés</h5>
+                                <p class="card-text"><a href="instr/accordion.php">Το Αραβικό Ακκορντεόν</a></p>
+                                <p class="card-text"><a href="instr/guitar.php">H Hλεκτρική Κιθάρα</a></p>
+                                <p class="card-text"><a href="instr/org.php">To Αραβικό Οργανο</a></p>
+                                <p class="card-text"><a href="instr/keyboard.php">To Αραβικό Αρμόνιο</a></p>
+                                <p class="card-text"><a href="instr/piano.php">To Αραβικό Πιάνο</a></p>
+                                <p class="card-text"><a href="instr/saxophone.php">Το Σαξόφωνο</a></p>
+                                <p class="card-text"><a href="instr/trumpet.php">Η Τρομπέτα</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
+                        <div class="card">
+                            <img class="card-img-top" src="/img/vergara.png" alt="Oud Rosette Vergara">
+                            <div class="card-body">
+                                <h5 class="card-title">Instruments de Percussion</h5>
+                                <p class="card-text"><a href="instr/riqq.php">Το Ρεκ</a></p>
+                                <p class="card-text"><a href="instr/tabla.php">Η Τάμπλα</a></p>
+                                <p class="card-text"><a href="instr/tabl_baladi.php">Η Τάμπλα Μπάλαντι</a></p>
+                                <p class="card-text"><a href="instr/daff.php">Το Νταφ</a></p>
+                                <p class="card-text"><a href="instr/mazhar.php">Το Μάζχαρ</a></p>
+                                <p class="card-text"><a href="instr/katim.php">Το Κάτεμ</a></p>
+                                <p class="card-text"><a href="instr/sagat.php">Τα Σαγκάτ</a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                <div class="card">
-                    <img class="card-img-top" src="/img/nahat_girgi.png" alt="Oud Rosette Nahhat">
-                    <div class="card-body">
-                        <h5 class="card-title">Instruments Arabisés</h5>
-                        <p class="card-text"><a href="instr/accordion.php">Το Αραβικό Ακκορντεόν</a></p>
-                        <p class="card-text"><a href="instr/guitar.php">H Hλεκτρική Κιθάρα</a></p>
-                        <p class="card-text"><a href="instr/org.php">To Αραβικό Οργανο</a></p>
-                        <p class="card-text"><a href="instr/keyboard.php">To Αραβικό Αρμόνιο</a></p>
-                        <p class="card-text"><a href="instr/piano.php">To Αραβικό Πιάνο</a></p>
-                        <p class="card-text"><a href="instr/saxophone.php">Το Σαξόφωνο</a></p>
-                        <p class="card-text"><a href="instr/trumpet.php">Η Τρομπέτα</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
-                <div class="card">
-                    <img class="card-img-top" src="/img/vergara.png" alt="Oud Rosette Vergara">
-                    <div class="card-body">
-                        <h5 class="card-title">Instruments de Percussion</h5>
-                        <p class="card-text"><a href="instr/riqq.php">Το Ρεκ</a></p>
-                        <p class="card-text"><a href="instr/tabla.php">Η Τάμπλα</a></p>
-                        <p class="card-text"><a href="instr/tabl_baladi.php">Η Τάμπλα Μπάλαντι</a></p>
-                        <p class="card-text"><a href="instr/daff.php">Το Νταφ</a></p>
-                        <p class="card-text"><a href="instr/mazhar.php">Το Μάζχαρ</a></p>
-                        <p class="card-text"><a href="instr/katim.php">Το Κάτεμ</a></p>
-                        <p class="card-text"><a href="instr/sagat.php">Τα Σαγκάτ</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
 
                 </div>
                 <div class="col-sm-12">
---
+++ el/sidemenu-instr.php
@@ -0,0 +1,23 @@
+<?php defined('_DEFVAR') or exit('Restricted Access'); ?>
+<ul class="sub-menu">
+    <li class="d-block d-sm-none"><a href="#" class="hide-side-menu"><i class="fa fa-fw fa-times"></i></a></li>
+    <li><a href="/el/instr/violin.php">Το Bιολί</a></li>
+    <li><a href="/el/instr/accordion.php">Το Αραβικό Ακκορντεόν</a></li>
+    <li><a href="/el/instr/keyboard.php">To Αραβικό Αρμόνιο</a></li>
+    <li><a href="/el/instr/qanun.php">Το Κανονάκι</a></li>
+    <li><a href="/el/instr/katim.php">Το Κάτεμ</a></li>
+    <li><a href="/el/instr/guitar.php">H Hλεκτρική Κιθάρα</a></li>
+    <li><a href="/el/instr/mazhar.php">Το Μάζχαρ</a></li>
+    <li><a href="/el/instr/buzuq.php">Το Μπούζουκ</a></li>
+    <li><a href="/el/instr/nay.php">Το Νάι</a></li>
+    <li><a href="/el/instr/daff.php">Το Νταφ</a></li>
+    <li><a href="/el/instr/org.php">To Αραβικό Οργανο</a></li>
+    <li><a href="/el/instr/oud.php">Το Ούτι</a></li>
+    <li><a href="/el/instr/piano.php">To Αραβικό Πιάνο</a></li>
+    <li><a href="/el/instr/riqq.php">Το Ρεκ</a></li>
+    <li><a href="/el/instr/sagat.php">Τα Σαγκάτ</a></li>
+    <li><a href="/el/instr/saxophone.php">Το Σαξόφωνο</a></li>
+    <li><a href="/el/instr/tabla.php">Η Τάμπλα</a></li>
+    <li><a href="/el/instr/tabl_baladi.php">Η Τάμπλα Μπάλαντι</a></li>
+    <li><a href="/el/instr/trumpet.php">Η Τρομπέτα</a></li>
+</ul>
