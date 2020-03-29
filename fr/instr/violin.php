5c03362d99711529989ef7f2d6489790eb1d5ead
Farraj <Johnny.Farraj@thomsonreuters.com>
2020-03-29 09:21:49 -0400

    french mistake

--- fr/instr/violin.php
+++ fr/instr/violin.php
@@ -4,7 +4,7 @@
 include('../../inc/config.php');
 
 /* SEO settings for this page */
-$page_title = "Le Violoin Arabe";
+$page_title = "Le Violon Arabe";
 $page_description = "Le Violon Arabe";
 $page_keywords = "arabe, musique, instruments, accordéon, bouzouk, daff, mazhar, guitare électrique, katèm, nay, synthétiseur oriental, orgue électrique, oud, ‘ud, piano, qanun, riqq, sagat, sajat, saxophone, tabla, tabl baladi, trompette, violon, monde arabe, maqam, jins, iqa‘, iqaa, rythme, quart de ton, tetrachord, modal, moyen orient, tarab";
 $page_language = "fr";
