53db9aabbf8f0b74a1a040744aa9a1d2aeddda01
Farraj <Johnny.Farraj@thomsonreuters.com>
2020-04-11 17:29:16 -0400

    english fix

--- en/iqaa/samai_saraband.php
+++ en/iqaa/samai_saraband.php
@@ -43,7 +43,7 @@ include($ROOT . 'inc/head.php');
                         <img src="/note/iqaa/samai_saraband.png" class="img-fluid">
                     </div>
                     
-                    <p>Iqa‘ Sama‘i Saraband, also called <strong>Sama‘i Ta’ir</strong> ("ta’ir" means flying in Arabic), is a light and fast <em>iqa‘</em> that is popular <a href="../form/vocal_comp.php#muwashah">Muwashah</a> vocal genre.</p>
+                    <p>Iqa‘ Sama‘i Saraband, also called <strong>Sama‘i Ta’ir</strong> ("ta’ir" means flying in Arabic), is a light and fast <em>iqa‘</em> that is popular in the <a href="../form/vocal_comp.php#muwashah">Muwashah</a> vocal genre.</p>
                     
                     <div class="player-area">
                         <audio id="player" controls>
