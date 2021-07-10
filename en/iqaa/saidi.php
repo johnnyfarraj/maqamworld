12329ba55e089a15b65906fccdaeb85775d7c4bd
Johnny Farraj  MacBook <johnnyfarraj@gmail.com>
2021-07-09 22:04:53 -0400

    spelling mistake

--- en/iqaa/saidi.php
+++ en/iqaa/saidi.php
@@ -30,7 +30,7 @@ include($ROOT . 'inc/head.php');
                     
                     <div class="heading jins text-center">
                         <h1>Iqa‘ Sa‘idi 4/4</h1>
-                        <h3>(also apelled Saidi)</h3>
+                        <h3>(also spelled Saidi)</h3>
                     </div>
                     <a href="#" class="show-side-menu d-block d-md-none btn btn-primary"><i class="fa fa-fw fa-bars"></i> Iqa‘ Index</a>
                     <div class="clearfix"></div>
